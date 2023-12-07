f63f44e4bca82bdf39eee5a904ec2eb2016ed5d4
kSuroweczka <92479853+kSuroweczka@users.noreply.github.com>
2023-12-07 21:08:23 +0100

    Home Screen

--- app/src/main/java/com/pawlowski/temperaturemanager/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/pawlowski/temperaturemanager/ui/screens/home/HomeScreen.kt
@@ -1,9 +1,9 @@
 package com.pawlowski.temperaturemanager.ui.screens.home
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absolutePadding
@@ -23,12 +23,14 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.pawlowski.temperaturemanager.R
@@ -40,15 +42,14 @@ fun HomeScreen(
     onEvent: (HomeEvent) -> Unit,
 ) {
     ToolBox(onBackClick = {})
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize().padding(vertical = 60.dp)) {
         when (state.devicesOverviewResource) {
             is Resource.Success -> {
                 val devices = state.devicesOverviewResource.data
 
-                LazyColumn(modifier = Modifier.weight(weight = 1f).padding(vertical = 80.dp, horizontal = 15.dp), verticalArrangement = Arrangement.spacedBy(13.dp)) {
+                LazyColumn(modifier = Modifier.weight(weight = 1f).padding(horizontal = 15.dp), verticalArrangement = Arrangement.spacedBy(13.dp)) {
                     items(devices) {
-                        DeviceParameters(it.device.name, 32.0, 20.1) // // potem zmienic na parametr z czujnika
-//                        Text(text = "DeviceName ${it.device.name}")
+                        DeviceParameters(it.device.name, it.currentTemperature, it.currentSoilMoisture)
                     }
                 }
             }
@@ -61,17 +62,14 @@ fun HomeScreen(
                 Text(text = "Error")
             }
         }
-//        Canvas(modifier = Modifier.size(100.dp), onDraw = {
-//            drawCircle(color = Color.Red)
-//        })
 
         Button(
             shape = CircleShape,
             onClick = {
                 onEvent(HomeEvent.AddNewDeviceClick)
             },
-            modifier = Modifier.absolutePadding(left = 320.dp, bottom = 20.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF001D4B))
+            modifier = Modifier.padding(20.dp).align(Alignment.End),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF001D4B)),
         ) {
             Text(text = "+", fontWeight = FontWeight.SemiBold, fontSize = 40.sp)
         }
@@ -80,20 +78,13 @@ fun HomeScreen(
 
 @Composable
 fun ToolBox(onBackClick: () -> Unit) {
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(height = 56.dp)
             .background(color = Color(0xFF001D4B)),
-        horizontalArrangement = Arrangement.spacedBy(220.dp),
+        contentAlignment = Alignment.CenterEnd,
     ) {
-        Icon(
-            painter = painterResource(id = R.drawable.home),
-            contentDescription = null,
-            tint = Color.White,
-            modifier = Modifier
-                .clickable { onBackClick.invoke() }.size(100.dp).fillMaxWidth(),
-        )
         Icon(
             painter = painterResource(id = R.drawable.person),
             contentDescription = null,
@@ -105,24 +96,26 @@ fun ToolBox(onBackClick: () -> Unit) {
 }
 
 @Composable
-fun DeviceParameters(DeviceName: String, Temperature: Double, Density: Double) {
+fun DeviceParameters(deviceName: String, temperature: Int, density: Int) {
     Row(modifier = Modifier.fillMaxWidth().height(60.dp).clip(shape = RoundedCornerShape(size = 5.dp)).background(color = Color(0xFFD9E2FF)).padding(10.dp), horizontalArrangement = Arrangement.spacedBy(10.dp)) {
         Icon(painter = painterResource(id = R.drawable.device), contentDescription = null, tint = Color.Black, modifier = Modifier.size(50.dp))
-        Column(modifier = Modifier.height(50.dp).width(150.dp).clip(shape = RoundedCornerShape(size = 5.dp)).background(color = Color(0xFF3F4759))) {
+        Column(modifier = Modifier.height(50.dp).weight(1f).clip(shape = RoundedCornerShape(size = 5.dp)).background(color = Color(0xFF3F4759))) {
             Text(
-                DeviceName,
+                deviceName,
                 textAlign = TextAlign.Center,
                 color = Color.White,
                 modifier = Modifier
-                    .padding(horizontal = 10.dp, vertical = 10.dp)
+                    .padding(horizontal = 10.dp, vertical = 15.dp)
                     .fillMaxWidth(),
-                fontSize = 15.sp,
+                fontSize = 10.sp,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
             )
         }
         Column(modifier = Modifier.height(50.dp).width(60.dp).clip(shape = RoundedCornerShape(size = 5.dp)).background(color = Color(0xFF355CA8))) {
-            var Temp = Temperature.toString()
+            val temp = temperature.toString()
             Text(
-                Temp + "°C",
+                temp + "°C",
                 textAlign = TextAlign.Center,
                 color = Color.White,
                 modifier = Modifier
@@ -132,9 +125,9 @@ fun DeviceParameters(DeviceName: String, Temperature: Double, Density: Double) {
             )
         }
         Column(modifier = Modifier.height(50.dp).width(60.dp).clip(shape = RoundedCornerShape(size = 5.dp)).background(color = Color(0xFF355CA8))) {
-            var Dens = Density.toString()
+            val dens = density.toString()
             Text(
-                Dens + "%",
+                dens + "%",
                 textAlign = TextAlign.Center,
                 color = Color.White,
                 modifier = Modifier
