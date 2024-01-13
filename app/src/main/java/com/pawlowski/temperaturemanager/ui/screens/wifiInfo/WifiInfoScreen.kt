2a5184d3b6fd2978dcce5101fc379a9dd40e574f
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2024-01-13 21:14:55 +0100

    Fix imePadding in WifiInfoScreen

--- app/src/main/java/com/pawlowski/temperaturemanager/ui/screens/wifiInfo/WifiInfoScreen.kt
+++ app/src/main/java/com/pawlowski/temperaturemanager/ui/screens/wifiInfo/WifiInfoScreen.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -50,64 +53,100 @@ fun WifiInfoScreen(
     when (state) {
         is WifiInfoState.Initialising -> {
         }
+
         is WifiInfoState.Content -> {
-            Column(verticalArrangement = Arrangement.spacedBy(37.dp)) {
+            Column(
+                modifier =
+                    Modifier
+                        .imePadding(),
+            ) {
                 Toolbar(
-                    leading = Toolbar.ToolbarLeading.Back(
-                        onClick = {
-                            onEvent(WifiInfoEvent.BackClick)
-                        },
-                    ),
+                    leading =
+                        Toolbar.ToolbarLeading.Back(
+                            onClick = {
+                                onEvent(WifiInfoEvent.BackClick)
+                            },
+                        ),
                 )
                 Column(
-                    modifier = Modifier.padding(horizontal = 10.dp)
-                        .clip(shape = RoundedCornerShape(size = 12.dp))
-                        .background(color = Color(0xFF70778B))
-                        .fillMaxWidth().padding(horizontal = 5.dp, vertical = 5.dp),
-                    verticalArrangement = Arrangement.spacedBy(0.dp),
+                    verticalArrangement = Arrangement.spacedBy(space = 32.dp),
+                    modifier = Modifier.verticalScroll(rememberScrollState())
+                        .padding(top = 32.dp),
                 ) {
-                    Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-                        Icon(painter = painterResource(id = R.drawable.device), contentDescription = null, tint = Color.White, modifier = Modifier.size(50.dp))
-                        Text(text = state.chosenAdvertisement.name, color = Color.White, fontSize = 30.sp, modifier = Modifier.padding(vertical = 10.dp))
-                    }
-                    Text(text = "MAC: " + state.chosenAdvertisement.macAddress, color = Color.White, fontSize = 20.sp)
-                }
-                Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
-                    SingleInputColumn(
-                        ssidInput = state.ssidInput,
-                        header = "Wpisz SSID: ",
-                        label = "SSID",
-                        onValueChange = { onEvent(WifiInfoEvent.ChangeSsid(it)) },
-                    )
-                    if (state.ssidError != null) {
+                    Column(
+                        modifier =
+                            Modifier
+                                .padding(horizontal = 10.dp)
+                                .clip(shape = RoundedCornerShape(size = 12.dp))
+                                .background(color = Color(0xFF70778B))
+                                .fillMaxWidth()
+                                .padding(horizontal = 5.dp, vertical = 5.dp),
+                        verticalArrangement = Arrangement.spacedBy(0.dp),
+                    ) {
+                        Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.device),
+                                contentDescription = null,
+                                tint = Color.White,
+                                modifier = Modifier.size(50.dp),
+                            )
+                            Text(
+                                text = state.chosenAdvertisement.name,
+                                color = Color.White,
+                                fontSize = 30.sp,
+                                modifier = Modifier.padding(vertical = 10.dp),
+                            )
+                        }
                         Text(
-                            text = state.ssidError,
-                            color = Color.Red,
-                            modifier = Modifier.absolutePadding(left = 45.dp),
+                            text = "MAC: " + state.chosenAdvertisement.macAddress,
+                            color = Color.White,
+                            fontSize = 20.sp,
                         )
                     }
-                }
-                Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
-                    SingleInputColumn(
-                        state.passwordInput,
-                        "Wpisz Hasło: ",
-                        "Hasło",
-                        onValueChange = { onEvent(WifiInfoEvent.ChangePassword(it)) },
-                    )
-                    if (state.passwordError != null) {
-                        Text(state.passwordError, color = Color.Red, modifier = Modifier.absolutePadding(left = 45.dp))
+                    Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+                        SingleInputColumn(
+                            ssidInput = state.ssidInput,
+                            header = "Wpisz SSID: ",
+                            label = "SSID",
+                            onValueChange = { onEvent(WifiInfoEvent.ChangeSsid(it)) },
+                        )
+                        if (state.ssidError != null) {
+                            Text(
+                                text = state.ssidError,
+                                color = Color.Red,
+                                modifier = Modifier.absolutePadding(left = 45.dp),
+                            )
+                        }
                     }
-                }
-                if (state.isLoading) {
-                    CircularProgressIndicator(modifier = Modifier.absolutePadding(left = 200.dp))
-                }
-                Button(
-                    onClick = { onEvent(WifiInfoEvent.ContinueClick) },
-                    enabled = !state.isLoading,
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 90.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF001D4B)),
-                ) {
-                    Text("Continue", modifier = Modifier.padding(all = 10.dp), fontSize = 20.sp)
+                    Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+                        SingleInputColumn(
+                            state.passwordInput,
+                            "Wpisz Hasło: ",
+                            "Hasło",
+                            onValueChange = { onEvent(WifiInfoEvent.ChangePassword(it)) },
+                        )
+                        if (state.passwordError != null) {
+                            Text(
+                                state.passwordError,
+                                color = Color.Red,
+                                modifier = Modifier.absolutePadding(left = 45.dp),
+                            )
+                        }
+                    }
+                    if (state.isLoading) {
+                        CircularProgressIndicator(modifier = Modifier.absolutePadding(left = 200.dp))
+                    }
+                    Button(
+                        onClick = { onEvent(WifiInfoEvent.ContinueClick) },
+                        enabled = !state.isLoading,
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 90.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF001D4B)),
+                    ) {
+                        Text("Continue", modifier = Modifier.padding(all = 10.dp), fontSize = 20.sp)
+                    }
                 }
             }
         }
@@ -116,13 +155,19 @@ fun WifiInfoScreen(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun SingleInputColumn(ssidInput: String, header: String, label: String, onValueChange: (String) -> Unit) {
+private fun SingleInputColumn(
+    ssidInput: String,
+    header: String,
+    label: String,
+    onValueChange: (String) -> Unit,
+) {
     Column(modifier = Modifier.padding(horizontal = 10.dp)) {
         Text(
             text = header,
-            modifier = Modifier
-                .padding(horizontal = 10.dp)
-                .fillMaxWidth(),
+            modifier =
+                Modifier
+                    .padding(horizontal = 10.dp)
+                    .fillMaxWidth(),
             fontWeight = FontWeight.SemiBold,
             fontSize = 23.sp,
             textAlign = TextAlign.Center,
@@ -130,9 +175,11 @@ private fun SingleInputColumn(ssidInput: String, header: String, label: String, 
         TextField(
             text = ssidInput,
             label = label,
-            modifier = Modifier.fillMaxWidth()
-                .padding(horizontal = 32.dp),
-//                            .background(color = Color(0xFFE3E2E6)).padding(vertical = 5.dp),
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp),
+            //                            .background(color = Color(0xFFE3E2E6)).padding(vertical = 5.dp),
             colors = TextFieldDefaults.textFieldColors(containerColor = Color(0xFFE9E6EB)),
             onValueChange = onValueChange,
         )
@@ -150,7 +197,8 @@ private fun TextField(
 ) {
     var showPassword by remember { mutableStateOf(value = false) }
 
-    OutlinedTextField( // // isError, supportingText?
+    OutlinedTextField(
+        // // isError, supportingText?
         value = text,
         onValueChange = onValueChange,
         label = { Text(label) },
@@ -158,11 +206,12 @@ private fun TextField(
         colors = colors,
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        visualTransformation = if (showPassword) {
-            VisualTransformation.None
-        } else {
-            PasswordVisualTransformation()
-        },
+        visualTransformation =
+            if (showPassword) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
         trailingIcon = {
             if (showPassword) {
                 IconButton(onClick = { showPassword = false }) {
